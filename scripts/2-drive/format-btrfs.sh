0a403a29710add5419c978bc5ea000ce18a023b1
ssnow <162399710+angryguy-win@users.noreply.github.com>
2024-10-10 18:58:45 -0400

    test

--- scripts/2-drive/format-btrfs.sh
+++ scripts/2-drive/format-btrfs.sh
@@ -65,16 +65,16 @@ subvolumes_setup() {
 mounting() {
     local partition_root="$1"
     local mount_options="$2"
-    local command
+    local command=()
     # Convert SUBVOLUMES to an array
     local subvolumes=(${SUBVOLUMES//,/ }) # DO NOT "" it breaks the array
     local subvol
 
 
-    command+="mount -o $mount_options,subvol=@ $partition_root /mnt"
-    command+="mkdir -p /mnt/{home,var,tmp,.snapshots,boot/efi}"
-    command+="$(for subvol in "${subvolumes[@]}"; do echo "mount -o $mount_options,subvol=$subvol $partition_root /mnt/$subvol"; done)"
-    command+="mount -t vfat -L EFIBOOT /mnt/boot/efi"
+    command+=("mount -o $mount_options,subvol=@ $partition_root /mnt")
+    command+=("mkdir -p /mnt/{home,var,tmp,.snapshots,boot/efi}")
+    command+=("$(for subvol in "${subvolumes[@]}"; do echo "mount -o $mount_options,subvol=$subvol $partition_root /mnt/$subvol"; done)")
+    command+=("mount -t vfat -L EFIBOOT /mnt/boot/efi")
 
     execute_process "Mounting subvolumes btrfs" \
         --error-message "Mounting subvolumes btrfs failed" \
